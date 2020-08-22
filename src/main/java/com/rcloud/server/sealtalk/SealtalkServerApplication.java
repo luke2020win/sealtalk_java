44751e9ed0e47ac4f974d6073cf71aa1a7b5e84a
apple <weilinfeng.2008@163.com>
2020-08-22 15:35:57 +0800

    no message

--- src/main/java/com/rcloud/server/sealtalk/SealtalkServerApplication.java
+++ src/main/java/com/rcloud/server/sealtalk/SealtalkServerApplication.java
@@ -2,7 +2,9 @@ package com.rcloud.server.sealtalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -10,10 +12,15 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ServletComponentScan(basePackages = {"com.rcloud.server.sealtalk.filter"})
 @SpringBootApplication
 @MapperScan("com.rcloud.server.sealtalk.dao")
-public class SealtalkServerApplication {
-
+public class SealtalkServerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SealtalkServerApplication.class, args);
     }
 
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
